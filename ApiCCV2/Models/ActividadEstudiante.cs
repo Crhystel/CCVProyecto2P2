f43b5484071074bc92a253a971e67e9de2069438
Crhystel <crhystelvelasco213@gmail.com>
2024-11-30 18:23:09 -0500

    Modelos de api

---
+++ ApiCCV2/Models/ActividadEstudiante.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiCCV2.Models
+{
+    public class ActividadEstudiante
+    {
+        public int EstudianteId { get; set; }
+        public Estudiante Estudiante { get; set; }
+        public int ActividadId { get; set; }
+        public Actividad Actividad { get; set; }
+    }
+}
---
+++ ApiCCV2/Models/ActividadProfesor.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiCCV2.Models
+{
+    public class ActividadProfesor
+    {
+        public int ProfesorId { get; set; }
+        public Profesor Profesor { get; set; }
+        public int ActividadId { get; set; }
+        public Actividad Actividad { get; set; }
+    }
+}
