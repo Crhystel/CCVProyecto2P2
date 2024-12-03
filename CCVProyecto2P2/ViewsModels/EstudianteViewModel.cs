699812c37a8b8f868e436e80247a474d14c1a5ba
Crhystel <crhystelvelasco213@gmail.com>
2024-12-02 21:26:04 -0500

    configuracion de vista estudiantepara conectar a api, fallido todo

--- CCVProyecto2P2/ViewsModels/EstudianteViewModel.cs
+++ CCVProyecto2P2/ViewsModels/EstudianteViewModel.cs
@@ -1,18 +1,61 @@
 ﻿using CCVProyecto2P2.Models;
 using CCVProyecto2P2.Services.EstudianteService;
 using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace CCVProyecto2P2.ViewsModels
 {
-    public class EstudianteViewModel : IEstudiante
+    public class EstudianteViewModel : IEstudiante, INotifyPropertyChanged
     {
+        private Estudiante _estudiante;
+
+        public Estudiante Estudiante
+        {
+            get => _estudiante;
+            set
+            {
+                _estudiante = value;
+                OnPropertyChanged();
+            }
+        }
         public static readonly string baseUrl = "http://192.168.100.20:5000";
+
+        public ObservableCollection<RolEnum> Roles { get; } = new ObservableCollection<RolEnum>(Enum.GetValues<RolEnum>());
+        public ObservableCollection<GradoEnum> Grados { get; } = new ObservableCollection<GradoEnum>(Enum.GetValues<GradoEnum>());
+
+        public ICommand GuardarEstudianteCommand { get; }
+
         public EstudianteViewModel()
         {
-            
+            Estudiante = new Estudiante();
+            GuardarEstudianteCommand = new Command(async () => await GuardarEstudiante());
+
         }
+        private async Task GuardarEstudiante()
+        {
+            var success = await AddUpdateEstudianteAsync(Estudiante);
+            if (success)
+            {
+               
+                await App.Current.MainPage.DisplayAlert("Éxito", "Estudiante guardado correctamente", "OK");
+            }
+            else
+            {
+                
+                await App.Current.MainPage.DisplayAlert("Error", "No se pudo guardar el estudiante", "OK");
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public async Task<bool> AddUpdateEstudianteAsync(Estudiante estudiante)
         {
             string json = JsonConvert.SerializeObject(estudiante);
@@ -85,5 +128,6 @@ namespace CCVProyecto2P2.ViewsModels
             }
             return await Task.FromResult(estudianteList);
         }
+       
     }
 }
